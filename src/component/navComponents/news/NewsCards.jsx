a665f970ef343bc9afac1a8cdded362d7cc67f10
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-08-05 17:30:41 +0630

    loading is done

--- src/component/navComponents/news/NewsCards.jsx
+++ src/component/navComponents/news/NewsCards.jsx
@@ -5,12 +5,20 @@ import SideNews from "./SideNews";
 
 const NewsCards = ({ section }) => {
   const { data, isError, isLoading } = useTopStoryFetch(section);
-  console.log(data?.results)
   return (
     <div className=" w-full bg-white  dark:bg-gray-700 dark:text-gray-200 py-2 border-b-2 border-black">
       {isLoading ? (
         <>
-          <h1>loading...</h1>
+          <div className="animate-pulse p-4">
+          <div className="flex justify-center w-full">
+            <div className="relative w-full h-48 bg-gray-300"></div>
+          </div>
+          <div className="flex flex-col gap-2 px-2 py-3">
+            <div className="h-6 bg-gray-300 w-3/4"></div>
+            <div className="h-16 bg-gray-300 w-full"></div>
+            <div className="h-10 bg-gray-300 w-24"></div>
+          </div>
+        </div>
         </>
       ) : isError ? (
         <>
--- src/component/navComponents/news/sectionNews/SectionNewsCard.jsx
+++ src/component/navComponents/news/sectionNews/SectionNewsCard.jsx
@@ -4,9 +4,9 @@ import SectionMainNewsCard from "./SectionMainNewsCard";
 import SideNews from "../SideNews";
 
 const SectionNewsCard = ({ section, sectionPath }) => {
+  const skeletonArray = new Array(2).fill(0);
   const { data, isError, isLoading } = useTopStoryFetch(section);
   const opinion = data?.results?.filter((i) => i.section === "opinion");
-  console.log(opinion);
   const currentPagePath = data?.results?.filter((i) =>
     sectionPath.includes(i.section)
   );
@@ -21,35 +21,65 @@ const SectionNewsCard = ({ section, sectionPath }) => {
   }, {});
 
   return (
-    <div className=" flex gap-2 w-full border-b-2 border-black bg-white dark:bg-gray-700 py-2">
-      <div
-        className={` flex flex-col gap-2 ${
-          opinion?.length > 0 && "w-3/4"
-        }  px-2 py-2 border-r-2  border-black`}
-      >
-        {newsBySection &&
-          Object.entries(newsBySection).map(([section, i]) => (
-            <div key={section}>
-              <h1 className=" first-letter:uppercase text-lg font-bold dark:text-gray-200">
-                {section} News
-              </h1>
-              <div className=" flex flex-col gap-2">
-                {<SectionMainNewsCard news={i} />}
+    <>
+      {isLoading ? (
+        <div className=" flex flex-col gap-2 p-2">
+          {skeletonArray.map((_, index) => (
+            <div
+              className="flex gap-2 bg-white dark:bg-gray-500 dark:text-gray-200 animate-pulse"
+              key={index}
+            >
+              <div className="w-40 h-40 bg-gray-300"></div>
+              <div className="w-2/3 flex flex-col justify-center">
+                <div className="h-6 bg-gray-300 mb-2 w-3/4"></div>
+                <div className="h-4 bg-gray-300 w-full"></div>
+                <div className="h-4 bg-gray-300 mt-2 w-5/6"></div>
               </div>
             </div>
           ))}
-      </div>
-      {opinion?.length > 0 && (
+        </div>
+      ) : (
         <>
-          <div className=" w-1/4">
-            <h1 className=" first-letter:uppercase text-lg font-bold dark:text-gray-200">
-              opinions
-            </h1>
-            {data && <>{<SideNews page={section} data={opinion} />}</>}
-          </div>
+          {isError ? (
+            <>error</>
+          ) : (
+            <>
+              <div className=" flex gap-2 w-full border-b-2 border-black bg-white dark:bg-gray-700 py-2">
+                <div
+                  className={` flex flex-col gap-2 ${
+                    opinion?.length > 0 && "w-3/4"
+                  }  px-2 py-2 border-r-2  border-black`}
+                >
+                  {newsBySection &&
+                    Object.entries(newsBySection).map(([section, i]) => (
+                      <div key={section}>
+                        <h1 className=" first-letter:uppercase text-lg font-bold dark:text-gray-200">
+                          {section} News
+                        </h1>
+                        <div className=" flex flex-col gap-2">
+                          {<SectionMainNewsCard news={i} />}
+                        </div>
+                      </div>
+                    ))}
+                </div>
+                {opinion?.length > 0 && (
+                  <>
+                    <div className=" w-1/4">
+                      <h1 className=" first-letter:uppercase text-lg font-bold dark:text-gray-200">
+                        opinions
+                      </h1>
+                      {data && (
+                        <>{<SideNews page={section} data={opinion} />}</>
+                      )}
+                    </div>
+                  </>
+                )}
+              </div>
+            </>
+          )}
         </>
       )}
-    </div>
+    </>
   );
 };
 
