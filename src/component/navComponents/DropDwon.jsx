1f6a9ce54d0ecbb6eb260cbabbb98d130f01eec4
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-08-07 14:14:07 +0630

    drop down button is done

--- src/component/navComponents/DropDwon.jsx
+++ src/component/navComponents/DropDwon.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const DropDwon = () => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  console.log(isDropDownOpen);
   const mode = useSelector((state) => state.nav.mode);
   if (mode) {
     document.documentElement.classList.remove("dark");
@@ -10,41 +12,67 @@ const DropDwon = () => {
     document.documentElement.classList.add("dark");
   }
   return (
-    <div className="m-1 rounded-lg hs-dropdown [--trigger:hover] relative inline-flex">
+    <div className=" relative">
       <button
-        id="hs-dropdown-hover-event"
-        type="button"
-        className=" dark:bg-slate-400 hs-dropdown-toggle py-2 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-        aria-haspopup="menu"
-        aria-expanded="false"
-        aria-label="Dropdown"
+        className=" bg-gray-200 dark:bg-gray-600 py-2 px-1.5 rounded-lg"
+        onClick={() => setIsDropDownOpen(!isDropDownOpen)}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
+        {!isDropDownOpen ? (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </>
+        ) : (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </>
+        )}
       </button>
       <div
-        className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0  min-w-60 bg-white dark:bg-slate-400 shadow-md rounded-lg p-1 space-y-0.5 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full hidden z-50"
-        role="menu"
-        aria-orientation="vertical"
-        aria-labelledby="hs-dropdown-hover-event"
+        className={` bg-gray-200 dark:bg-gray-600 dark:text-white flex-col  ${
+          isDropDownOpen ? " absolute -bottom-100%" : " hidden"
+        } flex flex-col w-[300px] rounded-xl text-black z-50`}
       >
-        <NavLink className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 dark:text-white hover:bg-gray-100 focus:outline-none focus:bg-gray-100" to={"/"}>Home</NavLink>
-        <NavLink className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 dark:text-white hover:bg-gray-100 focus:outline-none focus:bg-gray-100" to={"/worldnews"}>World</NavLink>
-        <NavLink className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 dark:text-white hover:bg-gray-100 focus:outline-none focus:bg-gray-100" to={"/usnews"}>US</NavLink>
-        <NavLink className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 dark:text-white hover:bg-gray-100 focus:outline-none focus:bg-gray-100" to={"/sciencnews"}>Science</NavLink>
-        <NavLink className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 dark:text-white hover:bg-gray-100 focus:outline-none focus:bg-gray-100" to={"/artnews"}>Art</NavLink>
+        <NavLink className="px-2 py-2" to={"/"}>
+          Home
+        </NavLink>
+        <NavLink className="px-2 py-2" to={"/worldnews"}>
+          World
+        </NavLink>
+        <NavLink className="px-2 py-2" to={"/usnews"}>
+          US
+        </NavLink>
+        <NavLink className="px-2 py-2" to={"/sciencnews"}>
+          Science
+        </NavLink>
+        <NavLink className="px-2 py-2" to={"/artnews"}>
+          Art
+        </NavLink>
       </div>
     </div>
   );
--- src/component/navComponents/Nav.jsx
+++ src/component/navComponents/Nav.jsx
@@ -15,10 +15,10 @@ const Nav = () => {
   return (
     <>
       <div className=" sticky top-0 z-50 flex max-[500px]:justify-between items-center gap-2 dark:bg-slate-800 bg-white dark:text-white px-2">
-        <div className="w-fit max-[500px]:inline hidden">
+        <div className="w-fit max-[500px]:flex hidden py-1">
           <DropDwon />
         </div>
-        <div className=" max-[500px]:hidden flex items-center justify-start gap-4 px-2 py-2 w-5/6">
+        <div className=" max-[500px]:hidden flex items-center justify-start gap-4 px-2 py-3 w-5/6">
           <NavLink to={"/"}>Home</NavLink>
           <NavLink to={"/worldnews"}>World</NavLink>
           <NavLink to={"/usnews"}>US</NavLink>
